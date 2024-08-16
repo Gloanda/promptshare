7e4d1ea35c247bc367ba011952478f09fce9b162
Loanda Gunawan <loanda.gunawan@ui.ac.id>
2024-08-16 19:18:54 +0700

    Implement feed page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import Feed from "@/components/feed";
+
 function Home() {
   return (
     <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">Discover & Share
-        <br className="max-md:hidden"/>
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
-      <p className="desc text-center">PromptShares is an AI prompting tool for modern world to discover, create, and share creative promts</p>
+      <p className="desc text-center">
+        PromptShares is an AI prompting tool for modern world to discover,
+        create, and share creative prompts
+      </p>
+
+      <Feed />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
